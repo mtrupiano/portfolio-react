9434930af2aeac0314b466bba0c81a76d505f17f
mtrupiano <markt6749@gmail.com>
2021-02-24 17:48:00 -0800

    styling project components

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BoxArrowInRight, Github } from 'react-bootstrap-icons';
+
+import './style.css';
+
+import '../../pages/style.css';
 
 export default function index(props) {
     return (
@@ -7,17 +12,22 @@ export default function index(props) {
                 <div className='col-md-2'>
                     
                 </div>
-                <div className='col-md-8'>
+                <div className='col-md-8 mb-2'>
                     <h3 className='project-header'>{props.title}</h3>
                     {props.description}
                 </div>
                 <div className='col-md-2 pl-5 d-flex align-items-center'>
                     <div className='row'>
-                        <div className='col'>
-                            <a href={props.github}>Github</a>
+                        <div className='col mb-2'>
+                            <a className='btn btn-large' href={props.github} target='_blank'>
+                                <Github className='mr-2'/>Github
+                            </a>
                         </div>
                         <div className='col'>
-                            <a href={props.github}>Deployed</a>
+                            <a className='btn' href={props.github} target='_blank'>
+                                <BoxArrowInRight className='mr-2'/>
+                                Deployed
+                            </a>
                         </div>
                     </div>
                 </div>
