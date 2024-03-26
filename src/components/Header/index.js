fec9a5de68574491f89db35b2fbad6759822843a
Mark Trupiano <markt6749@gmail.com>
2024-03-26 06:48:47 -0700

    remove unused bootstrap import

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { Github, Linkedin, Envelope, FileText } from 'react-bootstrap-icons';
+import { Github, Linkedin, Envelope } from 'react-bootstrap-icons';
 
 import './style.css'
 
