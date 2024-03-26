fbdcbada61a37ad846bb98b7502c4b4c1bff4e03
Mark Trupiano <markt6749@gmail.com>
2024-03-26 06:48:08 -0700

    remove unused css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Switch,
+} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
